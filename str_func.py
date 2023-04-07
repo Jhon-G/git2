40255dd71131c305732b49d8c31ea029174ee22d
Ivan Gurov <obuchenie_gurov@mail.ru>
2023-04-07 18:13:35 +0300

    Add add to file str_func.py def letter_cap and doc strings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,18 @@
 def string_capitalize(string: str):
+    """
+    Make all char in string upper
+
+    :param string:
+    :return:
+    """
     return string.upper()
+
+
+def letter_cap(string: str):
+    """
+    Make 1 letter capitalize
+
+    :param string:
+    :return:
+    """
+    return string.capitalize()
